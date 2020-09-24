a2b7f865ae779ffce9d5bf8eea1b5fafc2a3fc7b
Larry <laguil.92@gmail.com>
2020-09-24 03:20:55 -0700

    SecondCommit

---
+++ src/app/satellite.ts
@@ -0,0 +1,10 @@
+export class Satellite {
+    constructor(name: string, type: string, orbitType: string, launchDate: string, operational: boolean){
+        
+    }
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+}
